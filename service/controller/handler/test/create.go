883a04fec3168fc826dfd10afb5211a0883032ec
kubasobon <wtty.fool@gmail.com>
2023-11-07 12:37:06 +0100

    controller implementation

--- service/controller/handler/test/create.go
+++ service/controller/handler/test/create.go
@@ -2,9 +2,27 @@ package test
 
 import (
 	"context"
+
+	"github.com/giantswarm/k8smetadata/pkg/label"
+	"github.com/giantswarm/microerror"
 )
 
 func (r *Handler) EnsureCreated(ctx context.Context, obj interface{}) error {
 	r.logger.Debugf(ctx, "KUBA: EnsureCreated: %+v", obj)
+	cluster, err := toCluster(obj)
+	if err != nil {
+		return microerror.Mask(err)
+	}
+
+	releaseVersion, ok := cluster.Labels[label.ReleaseVersion]
+	if !ok {
+		r.logger.Debugf(ctx, "could not determine release version because Cluster %q/%q does not have a %q label",
+			cluster.Namespace, cluster.Name, label.ReleaseVersion)
+		r.logger.Debugf(ctx, "cancelling resource")
+		return nil
+	}
+
+	// TODO: depending on release version ensure empty or filled ConfigMap
+
 	return nil
 }
