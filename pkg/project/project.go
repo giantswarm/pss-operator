a6eb7baeb57632166bdf53ffa34ae92ebbb83b90
taylorbot <dev@giantswarm.io>
2023-11-21 11:20:43 +0000

    Bump version to 0.2.1-dev

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "pss-operator"
 	source      = "https://github.com/giantswarm/pss-operator"
-	version     = "0.2.0"
+	version     = "0.2.1-dev"
 )
 
 func Description() string {
